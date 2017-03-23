0e2b3769f61a9cb5691024dd8bf5bd156d1c8464
jzwdsb <545373574@qq.com>
2017-03-23 20:34:45 +0800

    公有头文件

---
+++ common_use.h
@@ -0,0 +1,33 @@
+//
+// Created by manout on 17-3-12.
+//
+
+#ifndef LEETCODE_COMMON_USE_H
+#define LEETCODE_COMMON_USE_H
+
+#include <string>
+#include <vector>
+#include <stack>
+#include <iso646.h>
+#include <algorithm>
+#include <limits>
+
+using std::string;
+using std::vector;
+using std::stack;
+
+using std::reverse;
+using std::next;
+using std::prev;
+using std::reverse_iterator;
+using std::max;
+using std::find;
+using std::bind1st;
+using std::swap;
+using std::sort;
+using std::begin;
+using std::end;
+using std::distance;
+using std::min;
+
+#endif //LEETCODE_COMMON_USE_H
