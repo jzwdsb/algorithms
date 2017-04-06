58a384a79271e94b709d7939561f03d7ba3e3e84
jzwdsb <545373574@qq.com>
2017-04-06 22:40:46 +0800

    二叉树的中序遍历

---
+++ BiT_inorder_traversal.cpp
@@ -0,0 +1,42 @@
+//
+// Created by manout on 17-4-6.
+//
+
+#include "common_use.h"
+#include "Bi_node.h"
+/*
+ *      inorder traversal a BiT
+ */
+
+
+
+void inorder_traversal(TreeNode* root, vector<int>& ret)
+{
+    if(not root)return;
+    inorder_traversal(root->left, ret);
+    ret.push_back(root->val);
+    inorder_traversal(root->right, ret);
+}
+
+vector<int> inorder_traversal_(TreeNode* root)
+{
+    vector<int> ret;
+    stack<TreeNode*> path;
+    const TreeNode* p = root;
+    if(not root)return ret;
+    while(not path.empty() or root not_eq nullptr)
+    {
+        if(root not_eq nullptr)
+        {
+            path.push(root);
+            root = root->left;
+        }
+        else{
+            root = path.top();
+            path.pop();
+            ret.push_back(root->val);
+            root = root->right;
+        }
+    }
+    return ret;
+}
