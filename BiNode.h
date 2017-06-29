6bbfedfe2adc957d000bf5f8a8e82fde9b1b5540
jzwdsb <545373574@qq.com>
2017-06-29 22:01:42 +0800

    二叉树节点声明

---
+++ BiNode.h
@@ -0,0 +1,16 @@
+//
+// Created by manout on 17-6-29.
+//
+
+#ifndef ALOGRITHMS_BINODE_H
+#define ALOGRITHMS_BINODE_H
+
+struct BiNode
+{
+    int val;
+    BiNode* left;
+    BiNode* right;
+};
+
+
+#endif //ALOGRITHMS_BINODE_H
