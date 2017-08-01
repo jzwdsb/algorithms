39f4c9dc7ff2843a036c553ba668f02685d9084f
jzwdsb <545373574@qq.com>
2017-08-01 20:46:37 +0800

    使用正则表达式判断是否符合模式，只有两行

--- pat_pass.cpp
+++ pat_pass.cpp
@@ -2,3 +2,16 @@
 // Created by manout on 17-8-1.
 //
 
+#include <string>
+#include <boost/regex.hpp>
+
+using namespace boost;
+
+
+bool judge_regular(std::string &src,
+        const regex& pattern = regex(R"==(.*?(p*?a*?t*?).*?)==", regex::perl | regex::icase) )
+{
+    match_results result;
+    return regex_match(src.begin(), src.end(), result, pattern, match_not_dot_null);
+
+}
