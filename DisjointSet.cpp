f500cbbe0ad2e0ba11b6d4ecaf588ddae317856f
jzwdsb <545373574@qq.com>
2017-08-03 18:22:49 +0800

    又写了遍并查集，以为会用到

---
+++ DisjointSet.cpp
@@ -0,0 +1,63 @@
+//
+// Created by manout on 17-8-3.
+//
+
+#include "DisjointSet.h"
+
+DisjointSet::DisjointSet(size_t eleCount):set_count(eleCount), rank(eleCount, 1u), reTable(eleCount)
+{
+    while(eleCount != 0u)
+    {
+        --eleCount;
+        reTable[eleCount] = eleCount;
+    }
+}
+
+size_t DisjointSet::find_set(size_t x)
+{
+    if (reTable[x] not_eq x)
+    {
+        return reTable[x] = find_set(reTable[x]);
+    }
+    return reTable[x];
+
+}
+
+bool DisjointSet::isSameSet(size_t x, size_t y)
+{
+    return find_set(x) == find_set(y);
+
+}
+
+size_t DisjointSet::set_size(size_t x)
+{
+    return rank[find_set(x)];
+}
+
+DisjointSet &DisjointSet::unite_set(size_t x, size_t y)
+{
+    link_set(find_set(x), find_set(y));
+}
+
+size_t DisjointSet::count_set()
+{
+    return set_count;
+}
+
+void DisjointSet::link_set(size_t x, size_t y)
+{
+    if(x not_eq y)
+    {
+        if(rank[x] >= rank[y])
+        {
+            reTable[y] = x;
+            rank[x] += rank[y];
+        }
+        else
+        {
+            reTable[x] = y;
+            rank[y] += rank[x];
+        }
+        --set_count;
+    }
+}
---
+++ DisjointSet.h
@@ -0,0 +1,31 @@
+//
+// Created by manout on 17-8-3.
+//
+
+#ifndef ALOGRITHMS_DISJOINTSET_H
+#define ALOGRITHMS_DISJOINTSET_H
+
+
+#include <glob.h>
+#include <vector>
+
+class DisjointSet
+{
+    DisjointSet() = default;
+    explicit DisjointSet(size_t eleCount);
+
+    size_t find_set(size_t x);
+    bool isSameSet(size_t x, size_t y);
+    size_t set_size(size_t x);
+    DisjointSet& unite_set(size_t x, size_t y);
+    size_t count_set();
+
+private:
+    void link_set(size_t x, size_t y);
+    size_t  set_count;
+    std::vector<size_t> rank;
+    std::vector<size_t> reTable;
+};
+
+
+#endif //ALOGRITHMS_DISJOINTSET_H
