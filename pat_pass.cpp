94efb1b2c34b92882dc161adf123b4cd7b971cca
jzwdsb <545373574@qq.com>
2017-08-08 14:23:20 +0800

    为什么自动生成的模板会这么长，手动害怕

--- pat_pass.cpp
+++ pat_pass.cpp
@@ -10,7 +10,7 @@ using std::regex;
 bool judge_regular(std::string &src,
         const regex& pattern = regex(R"==(.*?(p*?a*?t*?).*?)==", regex::icase) )
 {
-    std::match_results<std::basic_string<char, std::char_traits<char>, std::allocator<char>>::iterator> result;
+    std::match_results<std::string::iterator> result;
     return regex_match(src.begin(), src.end(), result, pattern,
             std::regex_constants::match_not_null);
 
