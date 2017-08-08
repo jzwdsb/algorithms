7a7846d305f8e89c84fb3c1aa7dee40442f51d5a
jzwdsb <545373574@qq.com>
2017-08-08 19:24:44 +0800

    以单词为单位反转字符串

--- reverse_string.cpp
+++ reverse_string.cpp
@@ -13,13 +13,18 @@ using std::stringstream;
 string reverse_string(const string& sentence)
 {
     string buffer;
-    stringstream string_list;
+    stringstream string_split;
+    std::vector<string> string_list;
     string ret;
     // stringstream　会使用空格和换行符作为分割字符串
-    while(string_list << sentence)
+    while(string_split << sentence)
     {
-        string_list >> buffer;
-        ret += buffer;
+        string_split >> buffer;
+        string_list.push_back(buffer);
+    }
+    for (auto it = string_list.rbegin(); it not_eq string_list.rend(); ++it)
+    {
+        ret += *it;
     }
     return ret;
 }
