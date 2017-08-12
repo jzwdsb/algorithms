92417e854011f9918c8549a85ea39040696ded94
jzwdsb <545373574@qq.com>
2017-08-12 10:03:57 +0800

    使用stringstream的速度超过71.55%，注意stringstream要clear

--- reverse_integer.cpp
+++ reverse_integer.cpp
@@ -12,31 +12,24 @@ using std::stringstream;
 using std::cout;
 
 
-class Solution {
-public:
-    int reverse(int x) {
-        int flag;
-        long num;
-        stringstream ss;
-        string num_str, re_str;
-        flag = x >= 0 ? 1 : -1;
-        ss << x;
-        ss >> num_str;
-        re_str.assign(num_str.rbegin(), num_str.rend());
-        ss.clear();
-        ss << re_str;
-        ss >> num;
-        if (num > INT_MAX)
-        {
-            return 0;
-        }
-        return static_cast<int>(num * flag);
-    }
-};
-
-int main ()
+int reverse(int x)
 {
-    Solution s;
-    cout<<s.reverse(13132);
-    return 0;
+    int flag;
+    long num;
+    stringstream ss;
+    string num_str, re_str;
+    flag = x >= 0 ? 1 : -1;
+    ss << x;
+    ss >> num_str;
+    re_str.assign(num_str.rbegin(), num_str.rend());
+    ss.clear();
+    ss << re_str;
+    ss >> num;
+    if (num > INT_MAX)
+    {
+        return 0;
+    }
+    return static_cast<int>(num * flag);
 }
+
+
