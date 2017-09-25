72ec6c7450878c7962011e7fc981a4780c77d4f1
jzwdsb <545373574@qq.com>
2017-09-25 22:17:51 +0800

    longest common subsequence

---
+++ longest_common_subsequence.cpp
@@ -0,0 +1,29 @@
+//
+// Created by manout on 17-9-25.
+//
+
+#include "common_use.h"
+
+using Matrix = vector<vector<int>>;
+
+
+int longest_common_subsequence(string a, string b)
+{
+	Matrix mat(a.size() + 1, Matrix::value_type(b.size() + 1, 0));
+	
+	for (int i = 1; i <= a.size(); ++i)
+	{
+		for (int j = 1; j <= b.size(); ++i)
+		{
+			if(a[i] == b[j])
+			{
+				mat[i][j] = mat[i + 1][j + 1] + 1;
+			}
+			else
+			{
+				mat[i][j] = max(mat[i - 1][j], mat[i][j - 1]);
+			}
+		}
+	}
+	return mat[a.size()][b.size()];
+}
