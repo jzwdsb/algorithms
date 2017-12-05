a44fbf2b1527facf300f6c7c39a34d78086b1632
jzwdsb <545373574@qq.com>
2017-12-05 18:45:30 +0800

    partially right

--- pat_pass.cpp
+++ pat_pass.cpp
@@ -1,17 +1,66 @@
-//
-// Created by manout on 17-8-1.
-//
-
+#include <iostream>
 #include <string>
-#include <regex>
-#include <regex.h>
+bool judege(const std::string& str);
 
-using std::regex;
-bool judge_regular(std::string &src,
-        const regex& pattern = regex(R"==(A*?PA*?TA*?)==", regex::icase) )
+/**
+     str = A(n)PATA(n)
+     str = A(n)PA(b)TA(b * n)
+     应该是这样的
+*/
+
+bool judege(const std::string& str)
 {
-    std::match_results<std::string::iterator> result;
-    return regex_match(src.begin(), src.end(), result, pattern,
-            std::regex_constants::match_not_null);
+    int prefix = 0;
+    int infix = 0;
+    int postfix = 0;
+    std::string::const_iterator it = str.begin();
+    while (it != str.end() && *it == 'A' )
+    {
+            ++it;
+            ++prefix;
+    }
+    if (it == str.end() || *it != 'P')
+    {
+        return false;
+    }
+    ++it;
+    if(it == str.end() || *it != 'A')
+    {
+        return false;
+    }
+    infix = 1;
+    while (it != str.end() && *it == 'A')
+    {
+        ++it;
+        ++infix;
+    }
+    if (it == str.end() || *it != 'T')
+    {
+        return false;
+    }
+    ++it;
+    while (it != str.end() && *it == 'A')
+    {
+        ++it;
+        ++postfix;
+    }
+    if (it == str.end() && postfix == prefix * infix)
+    {
+        return true;
+    }
+    return false;
 
+}
+
+int main()
+{
+    int n;
+    std::string str;
+    std::cin >> n;
+
+    while (n--)
+    {
+        std::cin >> str;
+        std::cout << (judege(str) ? "YES" : "NO") <<std::endl;
+    }
 }
