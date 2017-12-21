8317960f12462c43c04878a081a2fd39d4ad0591
jzwdsb <545373574@qq.com>
2017-12-21 22:31:23 +0800

    little work

--- circle_area.cpp
+++
@@ -1,4 +0,0 @@
-//
-// Created by manout on 17-12-20.
-//
-
--- fibonacci_mod.cpp
+++ fibonacci_mod.cpp
@@ -2,3 +2,23 @@
 // Created by manout on 17-12-19.
 //
 
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+
+int main()
+{
+	int n;
+	int *ans;
+	std::cin >> n;
+	ans = new int [n + 2];
+	ans[1] = 1;
+	ans[2] = 1;
+	for (int i = 3; i < n + 1; ++i)
+	{
+		ans[i] = (ans[i - 1 ] + ans[i - 2]) % 10007;
+	}
+	std::cout << ans[n];
+	delete ans;
+}
