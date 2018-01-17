e01c809eb72e3a39a05520810e90fb41e74adf4e
jzwdsb <manout960920@outlook.com>
2018-01-17 22:40:20 +0800

    binary search, well, time limited exceeded

---
+++ binary_search.cpp
@@ -0,0 +1,54 @@
+//
+// Created by manout on 18-1-17.
+//
+
+#include <vector>
+
+using std::vector;
+
+/** the input S is ensured is in ordered*/
+
+bool bin_search_r(vector<int> &S, int low, int high, int key)
+{
+	if (low >= high)
+		return false;
+	int i = (low + high) / 2;
+	if (S[i] == key)
+		return true;
+	if (S[i] > key)
+	{
+		return bin_search_r(S, low, i, key);
+	}
+	else return bin_search_r(S, i, high, key);
+	
+}
+
+bool bin_search(vector<int>& S, int key)
+{
+	int left = 0;
+	int right = static_cast<int>(S.size());
+	while (left < right)
+	{
+		int mid = (left + right) / 2;
+		if (S[mid] == key)
+			return true;
+		if (S[mid] > key)
+			right = mid;
+		else left = mid;
+	}
+	return false;
+}
+
+int binary_search(int n, vector<int>& S, int q, vector<int>& T)
+{
+	int count = 0;
+	S.push_back(0);
+	for (int ele : T)
+	{
+		if (bin_search_r(S, 0, n, ele))
+		{
+			++count;
+		}
+	}
+	return count;
+}
