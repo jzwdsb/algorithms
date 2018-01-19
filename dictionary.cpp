2ddf293c8159b7b4a9408ffffb49f8e00ef15f77
jzwdsb <manout960920@outlook.com>
2018-01-19 22:01:24 +0800

    after using stl, there is no work to do

---
+++ dictionary.cpp
@@ -0,0 +1,33 @@
+//
+// Created by manout on 18-1-19.
+//
+
+#include <vector>
+#include <string>
+#include <set>
+
+using std::vector;
+using std::string;
+using std::pair;
+using std::set;
+
+/** Write a program of a simple dictionary which implements the following instructions
+ * 		1: insert str. insert a string into a dictionary
+ * 		2: find str. if the dictionary contains str, then print 'yes', otherwise print 'no'
+ * */
+
+vector<string> dictionaay(const vector<pair<string, string>>& input)
+{
+	vector<string> ans;
+	set<string> container;
+	for (const pair<string, string>& ins : input)
+	{
+		if (ins.first[0] == 'i')
+		{
+			container.insert(ins.second);
+		}else
+		{
+			ans.emplace_back(container.count(ins.second) == 1 ? "yes" : "no");
+		}
+	}
+}
