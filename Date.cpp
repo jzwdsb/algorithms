8fe2ffcd48d3c4a407ac8c4e4f422171bdf7ac67
jzwdsb <manout960920@outlook.com>
2018-02-27 20:31:54 +0800

    Date impelment

---
+++ Date.cpp
@@ -0,0 +1,95 @@
+//
+// Created by manout on 18-2-26.
+//
+
+#include "Date.h"
+
+Date Date::operator+(int d)
+{
+    Date ans(*this);
+    
+    while (d not_eq 0)
+    {
+        int rest;
+        int leap;
+        switch (ans.month)
+        {
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+                rest = 31 - ans.day;
+                if (d > rest)
+                {
+                    d -= 31;
+                    ans.month += 1;
+                }else
+                {
+                    ans.day += d;
+                    d = 0;
+                }
+                break;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                rest = 30 - ans.day;
+                if (d > rest)
+                {
+                    d -= 30;
+                    ans.month += 1;
+                }else
+                {
+                    ans.day += d;
+                    d = 0;
+                }
+                break;
+            case 2:
+                leap = isleaq(ans.year) ? 29 : 28;
+                rest = leap - ans.day;
+                if (d > rest)
+                {
+                    d -= leap;
+                    ans.month += 1;
+                }else
+                {
+                    ans.day += d;
+                    d = 0;
+                }
+                break;
+            case 12:
+                rest = 31 - ans.day;
+                if (d > rest)
+                {
+                    d -= 31;
+                    ans.month = 1;
+                    ans.year += 1;
+                }else
+                {
+                    ans.day += d;
+                }
+                
+                break;
+            default:
+                break;
+        }
+    }
+    
+    return ans;
+}
+
+std::ostream& operator<<(std::ostream &os, Date date)
+{
+    char buffer[20];
+    sprintf(buffer, "%4d-%02d-%02d", date.year, date.month, date.day);
+    os << buffer;
+    return os;
+}
+
+bool Date::isleaq(int y)
+{
+    return y % 4 == 0 and y % 100 not_eq 0 or y % 400 == 0;
+}
+
---
+++ Date.h
@@ -0,0 +1,29 @@
+//
+// Created by manout on 18-2-26.
+//
+
+#ifndef ALOGRITHMS_DATE_H
+#define ALOGRITHMS_DATE_H
+
+#include <ostream>
+
+class Date
+{
+public :
+    Date():year(0), month(0), day(0){}
+    Date(Date&) = default;
+    Date(int y, int m = 0, int d = 0):year(y), month(m), day(d){}
+    Date operator+(int d);
+    
+    friend std::ostream& operator << (std::ostream& os, Date date);
+    
+private :
+    
+    static bool isleaq(int y);
+    int year;
+    int month;
+    int day;
+};
+
+
+#endif //ALOGRITHMS_DATE_H
