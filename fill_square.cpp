24abad0a603fafdddada4aaeed7ede4afbdc7fc6
jzwdsb <manout960920@outlook.com>
2018-03-22 22:27:35 +0800

    add comment

--- fill_square.cpp
+++ fill_square.cpp
@@ -10,7 +10,7 @@ const int cols = 4;
 
 int mapp[10][10] = {-10};
 bool numv[15] = {false};
-int cou;
+int count;
 
 int dir[4][2] = {{0, -1}, {-1, -1}, {-1, 0}, {-1, 1}};
 
@@ -22,7 +22,7 @@ static bool check(int x, int y, int n)
         int ny = y + dir[i][1];
         if (nx >= 0 and nx < rows and ny >= 0 and ny <= cols)
         {
-            if (abs(mapp[nx][ny] -  n) == 1)
+            if (abs(mapp[nx][ny] -  n) ==  1)
             {
                 return false;
             }
@@ -33,12 +33,14 @@ static bool check(int x, int y, int n)
 
 static void dfs(int dep, int pos)
 {
+    /** 2, 3 是搜索末尾且为缺口, 当前状态成立, 计数 + 1 后返回*/
     if (dep == 2 and pos == 3)
     {
-        ++cou;
+        ++count;
         return ;
     }
     
+    /** 当前行搜索完毕, 从下一行第一列开始搜索*/
     if (pos >= cols)
     {
         dfs(dep + 1, 0);
@@ -50,7 +52,7 @@ static void dfs(int dep, int pos)
         {
             numv[i] = true;
             mapp[dep][pos] = i;
-            dfs(dep, pos + 1);
+            dfs(dep, pos + 1;
             mapp[dep][pos] = -10;
             numv[i] = false;
         }
@@ -59,6 +61,8 @@ static void dfs(int dep, int pos)
 
 int fill_square()
 {
+    count = 0;
+    /** 0, 1 是缺口, 跳过*/
     dfs(0, 1);
-    return cou;
+    return count;
 }
