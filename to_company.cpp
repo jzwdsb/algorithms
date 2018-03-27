e141dcb8482b885dc6f1f88a0f70096e39002a92
jzwdsb <manout960920@outlook.com>
2018-03-27 08:25:01 +0800

    add abs in sum

--- to_company.cpp
+++ to_company.cpp
@@ -15,18 +15,18 @@ int TaxiTime(Point office, vector<Point>& taxis,int walktime,  int taxitime)
 {
     auto cond = [&](Point& a, Point& b)
     {
-        return abs(a.sum()) < abs(b.sum());
+        return a.sum() < b.sum();
     };
     
     sort(taxis.begin(), taxis.end(), cond);
-    int totaxi_time = walktime * abs(taxis[0].sum());
-    int taxi_time = taxitime * abs((office - taxis[0]).sum());
+    int totaxi_time = walktime * taxis[0].sum();
+    int taxi_time = taxitime * (office - taxis[0]).sum();
     return totaxi_time + taxi_time;
 }
 
 int WalkTime(Point office, int walk)
 {
-    return walk * abs(office.sum());
+    return walk * office.sum();
 }
 
 int to_company(int n, vector<Point>& taxis, Point office, int walktime, int taxitime)
